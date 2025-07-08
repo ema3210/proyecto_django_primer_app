f1ea386594b7b9d5a233d17387eba31e0a58c619
Emanuel Morales <ema.mrs@hotmail.com>
2025-07-07 23:35:05 -0300

    todo listo  para la entrega , se creo las 3 clases los forms de creacion y busqueda

--- Ejemplo_django/mi_primer_app/urls.py
+++ Ejemplo_django/mi_primer_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import saludo
+from .views import crear_familiar,inicio,crear_curso,crear_estudiante,buscar_cursos,cursos
 
 urlpatterns = [
-    path('Hola mundo/', saludo),
+    path('',inicio, name='inicio'), 
+    path('crear-familiar/', crear_familiar),
+    path('crear-curso/', crear_curso , name='crear-curso'),
+    path('crear-estudiante/', crear_estudiante , name='crear-estudiante'),
+    path('cursos/', cursos, name='cursos'),
+    path('cursos/buscar', buscar_cursos, name='buscar-cursos'),
 ]
--- Ejemplo_django/mi_primer_app/views.py
+++ Ejemplo_django/mi_primer_app/views.py
@@ -1,7 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+
+from .models import Familiar , Curso , Estudiante
+
+from .forms import FamiliarForm , CursoForm , EstudianteForm
 
 # Create your views here.
 from django.http import HttpResponse
 
+def inicio(request):
+    return render(request, 'mi_primer_app/inicio.html')
+
 def saludo(request):
     return HttpResponse('hola mundo')
+
+def saludo_con_template(request):
+    return render(request, 'mi_primer_app/saludo.html')
+
+def crear_familiar(request):
+    if request.method == 'POST':
+        form = FamiliarForm(request.POST)
+        if form.is_valid():
+            nuevo_curso = Familiar(
+                nombre=form.cleaned_data['nombre'],
+                apellido=form.cleaned_data['apellido'],
+                edad=form.cleaned_data['edad'],
+                fecha_nacimiento = form.cleaned_data['fecha_nacimiento'],
+                parentesco = form.cleaned_data['parentesco']
+            )
+            nuevo_curso.save()
+            return redirect('inicio')
+            
+            
+    
+    else:
+        form = FamiliarForm()
+        return render(request, 'mi_primer_app/crear_familiar.html', {'form':form})
+        
+  
+    
+    
+def crear_curso(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            nuevo_curso = Curso(
+                nombre=form.cleaned_data['nombre'],
+                descripcion=form.cleaned_data['descripcion'],
+                duracion_semanas=form.cleaned_data['duracion_semanas'],
+                fecha_inicio=form.cleaned_data['fecha_inicio'],
+                activo=form.cleaned_data['activo']
+            )
+            nuevo_curso.save()
+            return redirect('inicio')
+    else:
+        form = CursoForm()
+        
+    return render(request, 'mi_primer_app/crear_curso.html', {'form': form})    
+   
+        
+        
+def crear_estudiante(request): 
+    if request.method == 'POST':
+        form = EstudianteForm(request.POST)
+        if form.is_valid():
+            nuevo_curso = Estudiante(
+                nombre=form.cleaned_data['nombre'],
+                apellido=form.cleaned_data['apellido'],
+                email=form.cleaned_data['email'],
+                edad=form.cleaned_data['edad'],
+                fecha_inscripcion=form.cleaned_data['fecha_inscripcion']
+            )
+            nuevo_curso.save()
+            return redirect('inicio')
+            
+            
+    
+    else:
+        form = EstudianteForm()
+        return render(request, 'mi_primer_app/crear_estudiante.html', {'form':form})
+
+    
+
+def cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'mi_primer_app/cursos.html', {'cursos': cursos})
+    
+    
+def buscar_cursos(request):
+    if request.method == 'GET':
+        nombre = request.GET.get('nombre','')
+        cursos = Curso.objects.filter(nombre__icontains=nombre)
+        return render(request, 'mi_primer_app/cursos.html', {'cursos':cursos, 'nombre':nombre})
+     
